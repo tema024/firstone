552b4390acce69b312ac15248506a60f3db8b12a
tema024 <mslvart24@gmail.com>
2024-11-01 16:00:22 +0300

    Update 1sem4.py

---
+++ 1sem/1sem4.py
@@ -0,0 +1,18 @@
+with open("input.txt", "r") as file:
+    lines = file.readlines()
+    numbers = list(map(int, lines[0].split()))
+    op = lines[1].strip()
+
+if op == "+":
+    result = sum(numbers)
+elif op == "-":
+    result = numbers[0]
+    for n in numbers[1:]:
+        result -= n
+elif op == "*":
+    result = 1
+    for n in numbers:
+        result *= n
+
+with open("output.txt", "w") as file:
+    file.write(str(result))
