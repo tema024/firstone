5c51611175a683e86189b2c63f19f001697d5c4e
tema024 <mslvart24@gmail.com>
2024-12-15 02:07:49 +0300

    Update 3sem.1.py

--- 3sem/3sem.1.py
+++ 3sem/3sem.1.py
@@ -1,11 +1,18 @@
 def fib(n):
-    if n==0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-       res = fib(n-1) + fib(n-2)
-    return res
+    mem = [-1] * (n + 1)
+    def help(x):
+        if mem[x] != -1:
+            return mem[x]
+        if x == 0:
+            mem[x] = 0
+        elif x == 1:
+            mem[x] = 1
+        else:
+            mem[x] = help(x - 1) + help(x - 2)
+        return mem[x]
+
+    return help(n)
+
 n = int(input())
 res = fib(n)
 print(res)
