c819b165445150af6c84e91be3a0f81cc38f39f6
tema024 <mslvart24@gmail.com>
2024-12-16 02:06:12 +0300

    Create Caeser.py

---
+++ Caeser.py
@@ -0,0 +1,28 @@
+class Caesar:
+    alphabet = "яюэьыъщшчцхфутсрпонмлкйизжёедгвба"
+
+    def __init__(self, key):
+        self._encode = dict()
+        for i in range(len(self.alphabet)):
+            letter = self.alphabet[i]
+            encoded = self.alphabet[(i + key) % len(self.alphabet)]
+            self._encode[letter] = encoded
+            self._encode[letter.upper()] = encoded.upper()
+
+        self._decode = {v: k for k, v in self._encode.items()}
+
+    def encode(self, text):
+        return ''.join([self._encode.get(char, char) for char in text])
+
+    def decode(self, line):
+        return ''.join([self._decode.get(char, char) for char in line])
+
+
+
+key = int(input('Введите ключ:')) #19
+cipher = Caesar(key)
+
+line = input()
+while line:
+    print(cipher.encode(line))
+    line = input()
