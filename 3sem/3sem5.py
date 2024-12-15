09dd9ed339a2f43feaf0325d7346e33b6461c63f
tema024 <mslvart24@gmail.com>
2024-12-15 23:13:04 +0300

    Update 3sem5.py

--- 3sem/3sem5.py
+++ 3sem/3sem5.py
@@ -2,26 +2,33 @@ import numpy as np
 
 N = int(input())
 M = int(input())
+matrix = np.zeros((N, M), dtype=int)
 
-matrix = np.zeros((N,M), dtype = int)
-
-top, bottom = 0, N-1
-left, right = 0, M-1
+top, bottom = 0, N - 1
+left, right = 0, M - 1
 n = 1
-while top<=bottom and left<=right:
-    for i in range(left,right + 1):
+
+while top <= bottom and left <= right:
+    for i in range(left, right + 1):
         matrix[top][i] = n
         n += 1
-    top +=1
-    for i in range(top,bottom+1):
+    top += 1
+    for i in range(top, bottom + 1):
         matrix[i][right] = n
         n += 1
     right -= 1
-if top<=bottom:
-    for i in range(right,left -1, -1):
-        matrix[i][left] = n
-        n += 1
-    left += 1
+    if top <= bottom:
+        for i in range(right, left - 1, -1):
+            matrix[bottom][i] = n
+            n += 1
+        bottom -= 1
+    if left <= right:
+        for i in range(bottom, top - 1, -1):
+            matrix[i][left] = n
+            n += 1
+        left += 1
 
+for i in range(N):
+    matrix[i] *= (i + 1)  
 
 print(matrix)
